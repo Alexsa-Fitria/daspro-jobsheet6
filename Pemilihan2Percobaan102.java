766e6ca485c0571ef39c9d6b78cca4f4b73cc66d
alexsa <lexafitriayu@gmail.com>
2024-10-04 09:15:42 +0700

    Update Pemilihan2Percobaan102.java

--- Pemilihan2Percobaan102.java
+++ Pemilihan2Percobaan102.java
@@ -5,11 +5,17 @@ public class Pemilihan2Percobaan102 {
         System.out.print("Masukkan tahun: ");
         int tahun = absen02.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+        if (tahun % 100 == 0) {
+            if (tahun % 400 == 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else if (tahun % 4 == 0) {
             System.out.println("Tahun Kabisat");
-        } else
-        System.out.println("Bukan Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
         absen02.close();
     }
 }
